a17bfdba39b2a615f26cc520d90c23e4bf432441
Pranav Bhatia <30525912+PranavBhatia@users.noreply.github.com>
2022-06-20 00:55:36 -0400

    Create Logical Variables and Operators.R

---
+++ Logical Variables and Operators.R
@@ -0,0 +1,26 @@
+45 < 40
+15 < 30
+
+# ==
+# !=
+# >
+# <
+# <=
+# >=
+# !
+# |
+# &
+# isTRUE(x)
+
+result <- 4 < 5
+result
+typeof(result)
+
+result2 <- !TRUE
+result2
+
+result | result2
+result & result2
+
+isTRUE(result)
+isTRUE(result2)
